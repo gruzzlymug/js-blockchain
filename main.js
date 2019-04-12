e528e2c1095e6a57e431350808337d2fffa3e918
gruzzlymug <kevin@gruzzlymug.com>
2019-04-11 23:17:32 -0500

    Add project files

---
+++ main.js
@@ -0,0 +1,114 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
+class Block {
+  constructor(timestamp, transactions, previousHash = '') {
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
+
+  createGenesisBlock() {
+    return new Block("01/01/2018", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length -1];
+  }
+
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+
+    console.log("Block successfully mined!");
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ];
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
+        }
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; ++i) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+console.log("Mining block 1...");
+let xyz = new Blockchain();
+xyz.createTransaction(new Transaction('addr1', 'addr2', 100));
+xyz.createTransaction(new Transaction('addr2', 'addr1', 40));
+
+console.log('Starting the miner...');
+xyz.minePendingTransactions('dev-null');
+
+console.log('Balance of dev-null is ', xyz.getBalanceOfAddress('dev-null'));
+
+console.log('Starting the miner again...');
+xyz.minePendingTransactions('dev-null');
+
+console.log('Balance of dev-null is ', xyz.getBalanceOfAddress('dev-null'));
